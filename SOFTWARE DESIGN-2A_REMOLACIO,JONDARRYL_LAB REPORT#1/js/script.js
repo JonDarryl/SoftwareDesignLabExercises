d269932ca6c59c24cac7cc8218be8925b819deec
Jon Darryl Remolacio <101519101+JonDarryl@users.noreply.github.com>
2022-06-02 11:43:12 +0800

    Add files via upload

---
+++ SOFTWARE DESIGN-2A_REMOLACIO,JONDARRYL_LAB REPORT#1/js/script.js
@@ -0,0 +1,23 @@
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:250,
+    BackSpeed:250,
+    loop:true
+})
+
+const navBtn = document.querySelector('#navbar-toggler');
+const navDiv = document.querySelector('.navbar-collapse');
+
+navBtn.addEventListener('click', () => {
+    navDiv.classList.toggle('showNav');
+});
+
+// stopping animation and transition during window resizing
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
