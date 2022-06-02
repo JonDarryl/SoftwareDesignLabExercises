10daa979aeef5f12d98af68efd46b57c54fff5db
Jon Darryl Remolacio <101519101+JonDarryl@users.noreply.github.com>
2022-06-02 11:38:40 +0800

    Add files via upload

---
+++ SOFTWARE DESIGN-2A_REMOLACIO,JONDARRYL_LAB REPORT#3/2.a.iii.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def Atbegining(self, data_in):
+        NewNode = Node(data_in)
+        NewNode.next = self.head
+        self.head = NewNode
+
+    # Function to remove node
+    def RemoveNode(self, Removekey):
+        HeadVal = self.head
+
+        if (HeadVal is not None):
+            if (HeadVal.data == Removekey):
+                self.head = HeadVal.next
+                HeadVal = None
+                return
+        while (HeadVal is not None):
+            if HeadVal.data == Removekey:
+                break
+            prev = HeadVal
+            HeadVal = HeadVal.next
+
+        if (HeadVal == None):
+            return
+
+        prev.next = HeadVal.next
+        HeadVal = None
+
+    def LListprint(self):
+        printval = self.head
+        while (printval):
+            print(printval.data),
+            printval = printval.next
+
+
+llist = SLinkedList()
+llist.Atbegining("January")
+llist.Atbegining("February")
+llist.Atbegining("March")
+llist.Atbegining("April")
+llist.RemoveNode("February")
+llist.LListprint()
